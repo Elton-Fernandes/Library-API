b90d86524dab491cb8298da7c76c34c71eb850a2
Elton Fernandes <eltonfernandes.ef47@gmail.com>
2026-08-03 22:25:07 -0300

    feat(Editora): criação do CRUD de Editora

--- src/main/java/com/eltonfernandesdev/library_api/controller/EditoraController.java
+++ src/main/java/com/eltonfernandesdev/library_api/controller/EditoraController.java
@@ -1,9 +1,10 @@
 package com.eltonfernandesdev.library_api.controller;
 
-import com.eltonfernandesdev.library_api.repository.EditoraRepository;
+import com.eltonfernandesdev.library_api.model.Editora;
 import com.eltonfernandesdev.library_api.service.EditoraService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/library/editora")
@@ -13,4 +14,23 @@ public class EditoraController {
 
     public EditoraController(EditoraService editoraService) {this.editoraService = editoraService;}
 
+    @PostMapping
+    public Editora save(@RequestBody Editora editora) {
+        return editoraService.save(editora);
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Editora> findById(@PathVariable("id") Long idEditora) {
+        return editoraService.findById(idEditora);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable("id") Long idEditora) {
+        editoraService.deleteById(idEditora);
+    }
+
+    @PutMapping("/{id}")
+    public void alterById(@PathVariable("id") Long idEditora, @RequestBody Editora editora) {
+        editoraService.alterById(idEditora, editora);
+    }
 }
--- src/main/java/com/eltonfernandesdev/library_api/service/EditoraService.java
+++ src/main/java/com/eltonfernandesdev/library_api/service/EditoraService.java
@@ -1,7 +1,10 @@
 package com.eltonfernandesdev.library_api.service;
 
+import com.eltonfernandesdev.library_api.model.Editora;
 import com.eltonfernandesdev.library_api.repository.EditoraRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class EditoraService {
@@ -9,4 +12,21 @@ public class EditoraService {
     private EditoraRepository editoraRepository;
 
     public EditoraService(EditoraRepository editoraRepository) {this.editoraRepository = editoraRepository;}
+
+    public Editora save(Editora editora){
+        return editoraRepository.save(editora);
+    }
+
+    public Optional<Editora> findById(Long idEditora) {
+        return editoraRepository.findById(idEditora);
+    }
+
+    public void deleteById(Long idEditora) {
+        editoraRepository.deleteById(idEditora);
+    }
+
+    public void alterById(Long idEditora, Editora editora) {
+        editora.setIdEditora(idEditora);
+        editoraRepository.save(editora);
+    }
 }
