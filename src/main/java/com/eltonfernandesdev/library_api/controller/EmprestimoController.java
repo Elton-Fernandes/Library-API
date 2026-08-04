8c41d62ddafa80fb7d1489b4b900bdc74f60d5a4
Elton Fernandes <eltonfernandes.ef47@gmail.com>
2026-08-04 19:55:08 -0300

    feat(Emprestimo): criação de CRUD para Emprestimo

--- src/main/java/com/eltonfernandesdev/library_api/controller/EmprestimoController.java
+++ src/main/java/com/eltonfernandesdev/library_api/controller/EmprestimoController.java
@@ -1,8 +1,10 @@
 package com.eltonfernandesdev.library_api.controller;
 
+import com.eltonfernandesdev.library_api.model.Emprestimo;
 import com.eltonfernandesdev.library_api.service.EmprestimoService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/library/emprestimo")
@@ -11,4 +13,24 @@ public class EmprestimoController {
     private EmprestimoService emprestimoService;
 
     public EmprestimoController(EmprestimoService emprestimoService) {this.emprestimoService = emprestimoService;}
+
+    @PostMapping
+    public Emprestimo save(@RequestBody Emprestimo emprestimo) {
+        return emprestimoService.save(emprestimo);
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Emprestimo> findById(@PathVariable("id") Long idEmprestimo) {
+        return emprestimoService.findById(idEmprestimo);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable("id") Long idEmprestimo) {
+        emprestimoService.deleteById(idEmprestimo);
+    }
+
+    @PutMapping("/{id}")
+    public void alterById(@PathVariable("id") Long idEmprestimo, @RequestBody Emprestimo emprestimo) {
+        emprestimoService.alterById(idEmprestimo, emprestimo);
+    }
 }
--- src/main/java/com/eltonfernandesdev/library_api/service/EmprestimoService.java
+++ src/main/java/com/eltonfernandesdev/library_api/service/EmprestimoService.java
@@ -1,7 +1,10 @@
 package com.eltonfernandesdev.library_api.service;
 
+import com.eltonfernandesdev.library_api.model.Emprestimo;
 import com.eltonfernandesdev.library_api.repository.EmprestimoRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class EmprestimoService {
@@ -9,4 +12,21 @@ public class EmprestimoService {
     private EmprestimoRepository emprestimoRepository;
 
     public EmprestimoService(EmprestimoRepository emprestimoRepository) {this.emprestimoRepository = emprestimoRepository;}
+
+    public Emprestimo save(Emprestimo emprestimo) {
+        return emprestimoRepository.save(emprestimo);
+    }
+
+    public Optional<Emprestimo> findById(Long idEmprestimo) {
+        return emprestimoRepository.findById(idEmprestimo);
+    }
+
+    public void deleteById(Long idEmprestimo) {
+        emprestimoRepository.deleteById(idEmprestimo);
+    }
+
+    public void alterById(Long idEmprestimo, Emprestimo emprestimo) {
+        emprestimo.setIdEmprestimo(idEmprestimo);
+        emprestimoRepository.save(emprestimo);
+    }
 }
