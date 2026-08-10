93d5baf9de16ecc6fc0e5ab5aa0df89214e3b611
Elton Fernandes <eltonfernandes.ef47@gmail.com>
2026-08-09 22:23:56 -0300

    feat(Livro): adição de lógica que checa a existência da editora e dos autores, uso temporário do IllegalArgumentException

--- src/main/java/com/eltonfernandesdev/library_api/service/LivroService.java
+++ src/main/java/com/eltonfernandesdev/library_api/service/LivroService.java
@@ -1,19 +1,43 @@
 package com.eltonfernandesdev.library_api.service;
 
+import com.eltonfernandesdev.library_api.model.Autor;
 import com.eltonfernandesdev.library_api.model.Livro;
+import com.eltonfernandesdev.library_api.repository.AutorRepository;
+import com.eltonfernandesdev.library_api.repository.EditoraRepository;
 import com.eltonfernandesdev.library_api.repository.LivroRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
 public class LivroService {
 
     private LivroRepository livroRepository;
+    private EditoraRepository editoraRepository;
+    private AutorRepository autorRepository;
 
-    public LivroService(LivroRepository livroRepository) {this.livroRepository = livroRepository;}
+    public LivroService(LivroRepository livroRepository, EditoraRepository editoraRepository, AutorRepository autorRepository) {
+        this.livroRepository = livroRepository;
+        this.editoraRepository = editoraRepository;
+        this.autorRepository = autorRepository;
+    }
 
     public Livro salvar(Livro livro){
+        Long idEditora = livro.getEditora().getIdEditora();
+
+        if (!editoraRepository.existsById(idEditora)) {
+            throw new IllegalArgumentException("Editora não encontrada");
+        }
+
+        for (Autor autor : livro.getAutores()) {
+
+            Long idAutor = autor.getIdAutor();
+
+            if (!autorRepository.existsById(idAutor)) {
+                throw new IllegalArgumentException("Autor não encontrado: " + autor.getIdAutor());
+            }
+        }
         System.out.println("Livro salvo: " + livro);
         return livroRepository.save(livro);
     }
